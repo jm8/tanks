d9e9933b2c5cc4646a9d3bfdf7bc101d1e15c2e1
josh <jm8hjkl@gmail.com>
2022-11-22 13:24:47 -0500

    Make it look nice

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "FEHLCD.h"
 
 int main() {
-    LCD.WriteLine("Hello World!");
     while (1) {
+        LCD.Clear(0xff0000);
+        LCD.WriteLine("Hello world");
         LCD.Update();
         // Never end
     }
